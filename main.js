25425991540ede335f7f028f51912240e3a74cd9
joxxker <joxxker@MacBook-Pro-khxng-joxxker.local>
2023-06-30 20:15:05 +0700

    add inventory to keep amount of product

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ createApp({
         const description = ref('features to keep you warm in cold weather, or stylish socks for special occasions')
         const image = ref('./assets/images/socks_green.jpg')
         const url = ref('http://www.camt.cmu.ac.th.')
-        const inStock = ref(false)
+        const inStock = ref(true)
+        const inventory = ref(100)
         return{
             product, 
             description,
             image,
             url,
-            inStock
+            inStock,
+            inventory
         }
     }
 

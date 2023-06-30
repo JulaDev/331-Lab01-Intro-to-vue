95d8fccc01c1fdb01f1fec90a2ecd68363cc2d20
joxxker <joxxker@MacBook-Pro-khxng-joxxker.local>
2023-06-30 18:28:33 +0700

    2.5 add description

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const { createApp, ref } = Vue
 createApp({
     setup(){
         const product = ref('Boots')
+        const description = ref('features to keep you warm in cold weather, or stylish socks for special occasions')
         return{
-            product
+            product, description
         }
     }
 

1d22ac5a5431232467a03acfb67832c92a00d37b
joxxker <joxxker@MacBook-Pro-khxng-joxxker.local>
2023-06-30 19:03:25 +0700

    add new like image

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ createApp({
     setup(){
         const product = ref('Boots')
         const description = ref('features to keep you warm in cold weather, or stylish socks for special occasions')
+        const image = ref('./assets/images/socks_green.jpg')
         return{
-            product, description
+            product, 
+            description,
+            image
         }
     }
 
